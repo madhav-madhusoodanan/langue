508e79d317f60fdba3563d080e0e59fc611a1732
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-08-07 20:00:52 +0400

    implemented test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::collections::HashMap; 
 
 enum Command {
-    setVar(String, Value),
-    getVar(String)
+    SetVar(String, Value),
+    GetVar(String)
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 enum Value {
+    Nothing,
     Int(i64)
+}
+
+#[derive(Debug)]
+enum Error {
+    MissingVariable(String),
+
 }
 
 struct Evaluator {
@@ -15,29 +22,45 @@ struct Evaluator {
 }
 
 impl Evaluator {
-
-
-fn evaluate(&mut self, commands: &[Command]) -> Result<Value, Box<dyn std::error::Error>> {
-    for command in commands {
-        match command {
-            Command::setVar(name, value) => {
-                self.vars.insert(k: name.into(), v: value.clone())
-            }
+    fn new() -> Evaluator {
+        Self {
+            vars: HashMap::new(),
         }
     }
-}
 
+    fn evaluate(&mut self, commands: &[Command]) -> Result<Value, Error> {
+        let mut output = Ok(Value::Nothing);
+        for command in commands {
+            match command {
+                Command::SetVar(name, value) => {
+                    self.vars.insert(name.into(), value.clone());
+                }
+                Command::GetVar(name) => {
+                    match self.vars.get(name){
+                        Some(value) =>  output = Ok(value.clone()),
+                        None => return Err(Error::MissingVariable(name.into())),
+                    }
+                }
+            }
+        }
+        output
+    }
 }
 fn main () {
     
 }
 
 #[test]
-fn test_1 () {
+fn test_1 () -> Result<(), Error> {
     let commands = vec![
-        Command::setVar("a".into(), Value::Int(100)), 
-        Command::getVar("a".into())
+        Command::SetVar("a".into(), Value::Int(100)), 
+        Command::GetVar("a".into())
         ];
 
-    let result = evaluate(&commands);
+    let mut evaluator = Evaluator::new();
+    let result = evaluator.evaluate(&commands)?;
+
+    assert_eq!(result, Value::Int(100));
+
+    Ok(())
 }
